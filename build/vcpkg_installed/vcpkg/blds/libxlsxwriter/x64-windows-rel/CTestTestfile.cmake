f376089a546687aeaa6ab5681270fe6011a53841
carlosbenavides23 <carlosbenavides@gmail.com>
2026-08-27 18:43:28 -0600

    chore: ignore build and temporary files

--- build/vcpkg_installed/vcpkg/blds/libxlsxwriter/x64-windows-rel/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Carlos/source/repos/Contabilidad en C/build/vcpkg_installed/vcpkg/blds/libxlsxwriter/src/v1.2.4-916cd7d195.clean
-# Build directory: C:/Users/Carlos/source/repos/Contabilidad en C/build/vcpkg_installed/vcpkg/blds/libxlsxwriter/x64-windows-rel
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
